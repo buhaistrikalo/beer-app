2d5e115556ea3fd125bd36fc62c0a937243e170d
Kirill <kirilltcoi@yandex.ru>
2022-07-29 12:59:42 +0700

    config updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'images.punkapi.com',
+                    port: '',
+                    pathname: '/v2/**',
+                },
+            ],
+        },
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
